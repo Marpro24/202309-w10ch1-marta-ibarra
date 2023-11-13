ce04a85a3a664590f083ed663430234539078718
Marta Ibarra <martaa.ibarra@hotmail.com>
2023-11-13 19:42:40 +0100

    Fix configurations

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,11 @@
 import express from "express";
+import morgan from "morgan";
 
 const app = express();
+const port = process.env.PORT ?? 4000;
+
+app.listen(port, () => {
+  console.log(`listening`);
+});
+
+app.use(morgan("dev"));
