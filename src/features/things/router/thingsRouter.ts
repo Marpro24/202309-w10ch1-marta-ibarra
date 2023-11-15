769a9144eb8d76f19018ada745f482c35e8fe24e
Marta Ibarra <martaa.ibarra@hotmail.com>
2023-11-15 12:01:17 +0100

    Add new paths

--- src/features/things/router/thingsRouter.ts
+++ src/features/things/router/thingsRouter.ts
@@ -1,23 +1,11 @@
 import { Router } from "express";
-import things from "../data/things.js";
+import ThingsController from "../controller/ThingsController.js";
 
 const thingsRouter = Router();
+const thingsController = new ThingsController();
 
-thingsRouter.get("/", (_req, res) => {
-  res.status(200);
-  res.json({ things });
-});
+thingsRouter.get("/", thingsController.getThings);
 
-thingsRouter.get("/:idThing", (req, res) => {
-  const id = +req.params.idThing;
-
-  const thing = things.find((thing) => thing.id === id);
-
-  if (!thing) {
-    res.status(404).json();
-  }
-
-  res.status(200).json(thing);
-});
+thingsRouter.get("/:idThing", thingsController.getThingsById);
 
 export default thingsRouter;
